1c431b7d5f758219a16190e64ca69d1c97ad1dbb
Matthew Tondreau <mmtondreau@gmail.com>
2023-01-14 16:04:13 -0800

    ADd missing lambda configs

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -3,13 +3,20 @@ resource "aws_lambda_function" "option_quote_history" {
   role             = aws_iam_role.option_quote_history_lambda.arn
   filename         = "../dist/option_quote_history.zip"
   source_code_hash = filebase64sha256("../dist/option_quote_history.zip")
-  handler          = "option_quote_history.lambda_function.lambda_handler"
+  handler          = "newrelic_lambda_wrapper.handler"
   runtime          = "python3.9"
   publish          = true
   timeout          = 60
 
   environment {
     variables = {
+      # For the instrumentation handler to invoke your real handler, we need this value
+      NEW_RELIC_LAMBDA_HANDLER = "option_quote_history.lambda_function.lambda_handler"
+      NEW_RELIC_ACCOUNT_ID     = data.aws_ssm_parameter.newrelic_account_id.value
+      # Enable NR Lambda extension if the telemetry data are ingested via lambda extension
+      NEW_RELIC_LAMBDA_EXTENSION_ENABLED = true
+      # Enable Distributed tracing for in-depth monitoring of transactions in lambda (Optional)
+      NEW_RELIC_DISTRIBUTED_TRACING_ENABLED = true
       CODE : data.aws_ssm_parameter.code.value
       CLIENT_ID : data.aws_ssm_parameter.client_id.value
       STRIKE_COUNT : 75
